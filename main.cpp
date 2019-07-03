28a0fc29fb161786923a303b33f1b3bba4c6cd3d
Bilal Khan <bk@tinymanager.com>
2019-07-03 13:11:22 +0100

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "include/L2.h"
+#include "include/temp.h"
+
+int main()
+{
+    int x = 0;
+
+    int y = temp();
+
+    int z = temp2();
+
+    std::cout << "hello";
+
+    return 0;
+}
