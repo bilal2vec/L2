33ef6559e8973d9cabb832db6fa240c7b50d6310
Bilal Khan <bk@tinymanager.com>
2019-07-03 12:46:17 +0100

    Add temporary file structure

---
+++ L2/L2.cpp
@@ -0,0 +1,8 @@
+#include "L2.h"
+// use if manually compiling
+// #include "../include/L2.h"
+
+int temp()
+{
+    return 2;
+}
---
+++ L2/temp.cpp
@@ -0,0 +1,6 @@
+#include "temp.h"
+
+int temp2()
+{
+    return 1;
+}
